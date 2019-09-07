afbc3113c072737785c5ba68aa388ada6087a9ec
Tharlaw <Tharlaw@example.com>
2019-09-07 20:00:08 +0300

    improve form validation
    Updates validation for
    - username field
    - email address field
    - password field
    
    Update unit tests to check that validation messages are shown

--- petsrus/forms/forms.py
+++ petsrus/forms/forms.py
@@ -1,17 +1,26 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, EqualTo, Email
+from wtforms.validators import DataRequired, EqualTo, Email, Length
 
 
 class RegistrationForm(FlaskForm):
     username = StringField(
-        "Username", validators=[DataRequired(message="Please enter your username")]
+        "Username",
+        validators=[
+            DataRequired(message="Please enter your username"),
+            Length(
+                min=4,
+                max=25,
+                message="Username must be between 4 to 25 characters in length",
+            ),
+        ],
     )
     password = PasswordField(
         "Password",
         validators=[
             DataRequired(message="Please enter your password"),
             EqualTo("confirm_password", message="Passwords must match"),
+            Length(min=8, message="Password should be aleast 8 characters in length"),
         ],
     )
     confirm_password = PasswordField("Confirm password")
@@ -20,6 +29,7 @@ class RegistrationForm(FlaskForm):
         validators=[
             DataRequired(message="Please enter your email address"),
             Email(message="Please enter a valid email address"),
+            Length(min=6, max=35),
         ],
     )
     telephone = StringField("Telephone")
