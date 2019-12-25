9a7eae20dc374656517eea100d8d3c1208c9fed2
Tharlaw <Tharlaw@example.com>
2019-12-25 17:49:03 +0300

    Style register page
    - Introduce base.html to reduce duplication
    - Change form layout to be the same accross all forms
    - Display flash messages in register form page
    - Ensure that you get error messages when username or email already
    exists during registration
    - Update unit tests to check for duplicate usernames or email addresses

--- petsrus/views/main.py
+++ petsrus/views/main.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, request, url_for, flash
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import login_required, login_user, logout_user
 
+from sqlalchemy import exc
 from sqlalchemy.orm import sessionmaker
 
 from petsrus.petsrus import app, engine, login_manager
@@ -25,16 +26,27 @@ def register():
     form = RegistrationForm(request.form)
 
     if request.method == "POST" and form.validate():
-        user = User(
-            username=form.username.data,
-            password=generate_password_hash(form.password.data),
-            email_address=form.email_address.data,
-            telephone=form.telephone.data,
-            country=form.country.data,
-        )
-        db_session.add(user)
-        db_session.commit()
-        flash("Thanks for registering", "info")
+        try:
+            user = User(
+                username=form.username.data,
+                password=generate_password_hash(form.password.data),
+                email_address=form.email_address.data,
+                telephone=form.telephone.data,
+                country=form.country.data,
+            )
+            db_session.add(user)
+            db_session.commit()
+            flash("Thanks for registering", "info")
+        except exc.IntegrityError as error:
+            db_session.rollback()
+            app.logger.error(error.orig)
+            flash(
+                "Sorry, Username '{}' or Email '{}' is already in use".format(
+                    form.username.data, form.email_address.data
+                ),
+                "danger",
+            )
+            return render_template("register.html", form=form)
         return redirect(url_for("index"))
     else:
         return render_template("register.html", form=form)
